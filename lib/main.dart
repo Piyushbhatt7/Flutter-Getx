688f695ef5fe223a996b4d2608edc760bf05045b
vengersmoon <vengersmon003@gmail.com>
2024-12-28 01:10:46 +0530

    Update app theme to support light and dark modes

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,9 @@ class MyApp extends StatelessWidget {
     
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: const Dialog_alert(),
     );
   }
