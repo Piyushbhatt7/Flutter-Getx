4282c4b8a557a9889e4e8b9c57a0bdbc58a9e534
vengersmoon <vengersmon003@gmail.com>
2024-12-28 00:10:37 +0530

    g

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,7 +21,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
         ],
        ),
-
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        Get.snackbar("Pussy boy", "You deserve a pussy");
+      }),
       
     );
   }
