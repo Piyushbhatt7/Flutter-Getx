6b13a7dfcc80d7a24ebc9b227d69c01c34ad7249
vengersmoon <vengersmon003@gmail.com>
2024-12-28 11:05:06 +0530

    f

---
+++ lib/getx_navigator.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Getx_Navigator extends StatefulWidget {
+  const Getx_Navigator({super.key});
+
+  @override
+  State<Getx_Navigator> createState() => _Getx_NavigatorState();
+}
+
+class _Getx_NavigatorState extends State<Getx_Navigator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        title: Text("GetX Navigator"),
+        backgroundColor: Colors.amberAccent.shade200,
+       ),
+    );
+  }
+}
