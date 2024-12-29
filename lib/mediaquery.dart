bbe87a1673c4a5117a375860e5f18309a852ae66
vengersmoon <vengersmon003@gmail.com>
2024-12-29 12:12:22 +0530

    44

--- lib/mediaquery.dart
+++ lib/mediaquery.dart
@@ -19,7 +19,7 @@ class _Media_queryState extends State<Media_query> {
       body: Column(
         children: [
             Container(
-              height: 200,
+              height: MediaQuery.of(context).size.height,
               color: Colors.red,
               child: Center(
                 child: Text("Center"),
