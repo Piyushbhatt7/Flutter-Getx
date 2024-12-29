4d5e63c71ee18afd7c58f0008ac4c0e890921e02
vengersmoon <vengersmon003@gmail.com>
2024-12-29 22:06:58 +0530

    f

--- lib/statemanage.dart
+++ lib/statemanage.dart
@@ -1,49 +1,49 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class Statemanage extends StatefulWidget {
-  const Statemanage({super.key});
+// class Statemanage extends StatefulWidget {
+//   const Statemanage({super.key});
 
-  @override
-  State<Statemanage> createState() => _StatemanageState();
-}
+//   @override
+//   State<Statemanage> createState() => _StatemanageState();
+// }
 
-class _StatemanageState extends State<Statemanage> {
+// class _StatemanageState extends State<Statemanage> {
 
-int x = 0;
+// int x = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent.shade200,
-        title: Text('State Managment'),
-      ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.blueAccent.shade200,
+//         title: Text('State Managment'),
+//       ),
 
-      body: Column(
-        children: [
-          Center(
-            child: Text(x.toString(), style: TextStyle(fontSize: 50),),
-          ),
-          Expanded(child: ListView.builder(
-            itemCount: 1000,
-            itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(index.toString()),
-            );
-          }
+//       body: Column(
+//         children: [
+//           Center(
+//             child: Text(x.toString(), style: TextStyle(fontSize: 50),),
+//           ),
+//           Expanded(child: ListView.builder(
+//             itemCount: 1000,
+//             itemBuilder: (context, index) {
+//             return ListTile(
+//               title: Text(index.toString()),
+//             );
+//           }
           
-          ))
-        ],
-      ),
+//           ))
+//         ],
+//       ),
     
     
 
-      floatingActionButton: FloatingActionButton(onPressed: (){
-         x++;
-         setState(() {
+//       floatingActionButton: FloatingActionButton(onPressed: (){
+//          x++;
+//          setState(() {
            
-         });
-      }),
-    );
-  }
-}
+//          });
+//       }),
+//     );
+//   }
+// }
