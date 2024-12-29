3e5b10fc98231ee20a2bb1ca5a0370aa925a6150
vengersmoon <vengersmon003@gmail.com>
2024-12-29 15:15:15 +0530

    g

--- lib/languagechange.dart
+++ lib/languagechange.dart
@@ -2,7 +2,11 @@ import 'package:get/get_navigation/src/root/internacionalization.dart';
 
 class Language_change extends Translations{
   @override
-  Map<String, Map<String, String>> get keys => throw UnimplementedError();
+  Map<String, Map<String, String>> get keys => {
+    'en_US' : {
+      'message': 'What is you name'
+    }
+  };
 
   
 }
