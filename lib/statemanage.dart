9a003928087891306230875286e98be0ac011440
vengersmoon <vengersmon003@gmail.com>
2024-12-29 20:15:40 +0530

    g

---
+++ lib/statemanage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Statemanage extends StatefulWidget {
+  const Statemanage({super.key});
+
+  @override
+  State<Statemanage> createState() => _StatemanageState();
+}
+
+class _StatemanageState extends State<Statemanage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent.shade200,
+        title: Text('State Managment'),
+      ),
+    );
+  }
+}
