b620e39e7c71712893b0b9fdc75883066822394e
vengersmoon <vengersmon003@gmail.com>
2024-12-31 14:34:25 +0530

    g

--- lib/Favorite.dart
+++ lib/Favorite.dart
@@ -68,6 +68,11 @@ class Favorite extends StatefulWidget {
 class _FavoriteState extends State<Favorite> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        title: Text('Favorite'),
+      ),
+    );
   }
 }
