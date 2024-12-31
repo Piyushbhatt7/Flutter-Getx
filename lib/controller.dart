54f72543c4d65fd21ec4a14675e4443a2de23a60
vengersmoon <vengersmon003@gmail.com>
2024-12-31 14:38:10 +0530

    h

--- lib/controller.dart
+++ lib/controller.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 class ExampleController extends GetxController {
 
-      List<String> fruitList = ['Orange', 'Banana', 'Papya', 'Mango', 'Watermelon', 'Strawbery'].obs;
+      RxList<String> fruitList = ['Orange', 'Banana', 'Papya', 'Mango', 'Watermelon', 'Strawbery'].obs;
 
-      List<String> tempList = [].obs;
+      List<String> tempList = [];
 }
