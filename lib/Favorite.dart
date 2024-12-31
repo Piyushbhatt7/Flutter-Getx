136659f45f6a342623fd0a028f55eebb7fafa2c3
vengersmoon <vengersmon003@gmail.com>
2024-12-31 14:33:54 +0530

    g

--- lib/Favorite.dart
+++ lib/Favorite.dart
@@ -53,3 +53,21 @@
 //     );
 //   }
 // }
+
+
+
+import 'package:flutter/material.dart';
+
+class Favorite extends StatefulWidget {
+  const Favorite({super.key});
+
+  @override
+  State<Favorite> createState() => _FavoriteState();
+}
+
+class _FavoriteState extends State<Favorite> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
