6dda94283a0fb547a7db31c42b6d7b105df5e64b
vengersmoon <vengersmon003@gmail.com>
2025-01-01 00:36:05 +0530

    f

--- lib/imagepicker.dart
+++ lib/imagepicker.dart
@@ -26,10 +26,11 @@ import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class imagePicker extends GetxController {
-
+ RxString imagePath = ''.obs;
   Future getImage() async {
        
        final ImagePicker _picker = ImagePicker();
        final image = await _picker.pickImage(source: ImageSource.gallery);
+
   }
 }
