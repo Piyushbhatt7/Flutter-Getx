c36391df792262a2fe5dca3d146adc058e54b817
vengersmoon <vengersmon003@gmail.com>
2025-01-01 00:33:49 +0530

    f

--- lib/imagepicker.dart
+++ lib/imagepicker.dart
@@ -1,23 +1,30 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class imagePicker extends StatefulWidget {
-  const imagePicker({super.key});
+// class imagePicker extends StatefulWidget {
+//   const imagePicker({super.key});
 
-  @override
-  State<imagePicker> createState() => _imagePickerState();
-}
+//   @override
+//   State<imagePicker> createState() => _imagePickerState();
+// }
 
-class _imagePickerState extends State<imagePicker> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+// class _imagePickerState extends State<imagePicker> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
 
-        backgroundColor: Colors.blue,
-        title: Text('Image Picker'),
-      ),
+//         backgroundColor: Colors.blue,
+//         title: Text('Image Picker'),
+//       ),
 
-      body: ,
-    );
+//       body: ,
+//     );
+//   }
+// }
+
+class imagePicker extends GetxController {
+
+  Future getImage() async {
+    
   }
 }
