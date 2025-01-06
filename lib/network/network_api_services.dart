570eba79aa2fd2ec6acb6a9cf02191129f616847
vengersmoon <vengersmon003@gmail.com>
2025-01-06 22:57:14 +0530

    y

--- lib/network/network_api_services.dart
+++ lib/network/network_api_services.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter_getx/app_exeptions.dart';
 import 'package:flutter_getx/network/base_api_services.dart';
 import 'package:http/http.dart' as http;
 
@@ -5,6 +8,13 @@ class NetworkApiServices extends BaseApiServices {
     
     @override
     Future<dynamic> getApi(String url) async{
+
+      try {
+
+      }
+      on SocketException {
+         throw InternetException();
+      }
         final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
     }
 
