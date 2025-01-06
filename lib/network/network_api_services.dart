5520c981943b4df1bacbbb80054df0f5e74b6fa5
vengersmoon <vengersmon003@gmail.com>
2025-01-06 22:53:11 +0530

    j

--- lib/network/network_api_services.dart
+++ lib/network/network_api_services.dart
@@ -1,5 +1,10 @@
 import 'package:flutter_getx/network/base_api_services.dart';
 
 class NetworkApiServices extends BaseApiServices {
-      
+    
+    @override
+    Future<dynamic> getApi(String url) async{
+        
+    }
+
 }
