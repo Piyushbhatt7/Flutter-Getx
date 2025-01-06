f9c7632d4613c5ea1e74c3043d771b79207c539c
vengersmoon <vengersmon003@gmail.com>
2025-01-06 15:12:51 +0530

    h

--- lib/splash_scren.dart
+++ lib/splash_scren.dart
@@ -11,7 +11,10 @@ class _SplashScrenState extends State<SplashScren> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+
+      appBar: AppBar(
+        
+      ),
     );
   }
 }
