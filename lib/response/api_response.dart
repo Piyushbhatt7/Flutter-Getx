9e47e58a1a87344c8c408c26d3a4aa930dafafae
vengersmoon <vengersmon003@gmail.com>
2025-01-06 22:25:57 +0530

    l

--- lib/response/api_response.dart
+++ lib/response/api_response.dart
@@ -9,10 +9,13 @@ class ApiResponse <T> {
    ApiResponse(this.status, this.data, this.msg);
 
    ApiResponse.loading() : status : Status.LODAING ;
+   ApiResponse.completed() : status : Status.COMPLETED ;
+   ApiResponse.error() : status : Status.ERROR ;
+
    @override
 
    String toString(){
     return 'Status : $status \n Message : $msg \n Data : $data';
-    
+
    }
 }
