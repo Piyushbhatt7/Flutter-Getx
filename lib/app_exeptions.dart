7c83007a60a03865fe242c7bd163cc36d9060a50
vengersmoon <vengersmon003@gmail.com>
2025-01-06 15:16:31 +0530

    g

--- lib/app_exeptions.dart
+++ lib/app_exeptions.dart
@@ -1,3 +1,12 @@
 class AppExeptions implements Exception{ 
    
+   final _msg;
+   final _prefix;
+
+   AppExeptions({this._msg, this._prefix});
+
+   String toString(){
+
+    return '$_prefix$_msg'
+   }
 }
