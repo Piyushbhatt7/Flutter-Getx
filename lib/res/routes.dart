1328c59bc9a5266bd5c4570b8285767612a91992
vengersmoon <vengersmon003@gmail.com>
2025-01-07 10:52:13 +0530

    k

---
+++ lib/res/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_getx/res/routes_namne.dart';
+import 'package:flutter_getx/view/splash.dart';
+import 'package:get/get.dart';
+
+class AppRoutes {
+
+  static appRoutes () => [
+
+    GetPage(name : RoutesNamne.Splash, page: () =>  Splash(),)
+  ];
+}
