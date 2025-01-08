0bca21a06f84c21548526226c7b35ba832ac1d4d
vengersmoon <vengersmon003@gmail.com>
2025-01-08 09:59:20 +0530

    k

---
+++ lib/utils/utils.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class Utils {
+
+  static void fieldFocusChange(BuildContext context, FocusNode current, FocusNode nextFocus){
+
+    current.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+
+  }
+}
